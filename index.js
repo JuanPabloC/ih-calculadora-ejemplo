cd47ec564abbaf00f3275825e2c8936b43f75dc7
Juan Pablo Castano <jpcastanom@gmail.com>
2021-06-28 11:05:07 -0500

    desarrollo de funcionalidad

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 console.log("hola mundo")
+
+// 1. VARIABLES
+
+const nombre = document.getElementById("capturar-nombre")
+const btnSaludar = document.getElementById("btn-saludar")
+const areaSaludo = document.getElementById("area-saludo")
+
+
+// 2. FUNCIONES
+const saludar = (val) => {
+    const saludo = `Hola ${val}!!!`
+
+    areaSaludo.innerHTML = saludo
+}
+
+// 3. EVENTOS
+
+btnSaludar.addEventListener("click", () => {
+    const nameValue = nombre.value
+    saludar(nameValue)
+})
