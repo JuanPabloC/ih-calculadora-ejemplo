7b6d1954ff819ffede68ff0594c28b7fedf8aeb3
Juan Pablo Castano <jpcastanom@gmail.com>
2021-06-28 11:52:21 -0500

    agregamos funcion de resta

--- index.js
+++ index.js
@@ -1,30 +1,51 @@
 // 1. VARIABLES
-const nombre        = document.getElementById("capturar-nombre")
-const btnSaludar    = document.getElementById("btn-saludar")
-const areaSaludo    = document.getElementById("area-saludo")
+const nombre = document.getElementById("capturar-nombre");
+const btnSaludar = document.getElementById("btn-saludar");
+const areaSaludo = document.getElementById("area-saludo");
 
-const valorA        = document.getElementById("valor-a")
-const valorB        = document.getElementById("valor-b")
-const btnSumar      = document.getElementById("btn-sumar")
-const areaSuma      = document.getElementById("area-suma")
+const valorA = document.getElementById("valor-a");
+const valorB = document.getElementById("valor-b");
+const btnSumar = document.getElementById("btn-sumar");
+const areaSuma = document.getElementById("area-suma");
 
+const valorC = document.getElementById("valor-c");
+const valorD = document.getElementById("valor-d");
+const btnRestar = document.getElementById("btn-restar");
+const areaRestar = document.getElementById("area-restar");
 
 // 2. FUNCIONES
 const saludar = (name) => {
-    const saludo = `Hola ${name}!!!`
-    return areaSaludo.innerHTML = saludo
-}
+  const saludo = `Hola ${name}!!!`;
+  return (areaSaludo.innerHTML = saludo);
+};
 const sumar = (a, b) => {
-    const resultado = a + b
-    return areaSuma.innerHTML = resultado
-}
+  return a + b;
+};
+
+const restar = (a, b) => {
+  return a - b;
+};
+
 // 3. EVENTOS
 btnSaludar.addEventListener("click", () => {
-    const nameValue = nombre.value
-    return saludar(nameValue)
-})
+  const nameValue = nombre.value;
+  return saludar(nameValue);
+});
+
 btnSumar.addEventListener("click", () => {
-    const inputValorA   = parseInt(valorA.value)
-    const inputValorB   = parseInt(valorB.value)
-    return sumar(inputValorA, inputValorB)
-})
+  const inputValorA = parseInt(valorA.value);
+  const inputValorB = parseInt(valorB.value);
+
+  const resultado = sumar(inputValorA, inputValorB);
+
+  areaSuma.innerHTML = resultado;
+});
+
+btnRestar.addEventListener("click", () => {
+  const inputValorC = parseInt(valorC.value);
+  const inputValorD = parseInt(valorD.value);
+
+  const resultado = restar(inputValorC, inputValorD);
+
+  areaRestar.innerHTML = resultado;
+});
